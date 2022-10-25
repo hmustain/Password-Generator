276c423867d5560f6a3ef98da0cb92bfb72a9508
hmustain <hunter@bunkerbranding.com>
2022-10-25 14:05:41 -0500

    adding some global var, and assigning values to objects

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Global Variables 
+var confirm;
+var confirmNumber;
+var confirmLowercase;
+var confirmUppercase;
+var confirmCharacter;
+
+// Objects & Values
+// Numbers
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+// Lower case
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Upper Case
+upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Special Characters
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~", " "];
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
